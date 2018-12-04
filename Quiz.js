84430fceb1f79f8913b9bd039752b96d8cd0a242
Masroor-Unar <45050517+Masroor-Unar@users.noreply.github.com>
2018-12-04 14:25:23 +0000

    Created Vr button functionality for quiz

--- Quiz.js
+++ Quiz.js
@@ -1,57 +1,57 @@
-import React from 'react';
-import {
-    Text,
-    View,
-  } from 'react-vr';
-  
-  export default class Quiz extends React.Component{
-    static defaultProps = {
-        "rotateY":0,
-        "question":"",
-        "answer1":"",
-        "answer2":"",
-        "answer3":"",
-        "correct":""
-      };
-    
-      constructor(props) {
-        super();
-        this.state = {
-        };
-        this.textStyle = {
-            color: "#fff200",
-            fontWeight: "bold"
-        }
-      }
+import React from "react";
+import { Text, View, VrButton } from "react-vr";
 
-      render(){
-        return(
-            <View
-                style={{
-                    backgroundColor: "#3498db",
-                    position:'absolute',
-                    layoutOrigin: [0.5, 0.5, 0.5],
-                    width: 1,
-                    height: 0.5,
-                    transform: [
-                    {rotateY: this.props.rotateY},
-                    {translateZ: -3}
-                    ],
-                    opacity: 1,
-                }}>
-                <Text style={this.textStyle}>
-                    {this.props.question}
-                </Text>
-                <Text style={this.textStyle}>
-                    {this.props.answer1}
-                </Text>
-                <Text style={this.textStyle}>
-                    {this.props.answer2}
-                </Text>
-                <Text style={this.textStyle}>
-                    {this.props.answer3}
-                </Text>
-            </View>
-        )
-      }
+export default class Quiz extends React.Component {
+  static defaultProps = {
+    rotateY: 0,
+    question: "",
+    answer1: "",
+    answer2: "",
+    answer3: "",
+    correct: ""
+  };
+
+  constructor(props) {
+    super();
+    state = {
+      textStyle: {
+        color: "#fff200",
+        fontWeight: "bold"
+      },
+      output: ""
+    };
+    // bind here
   }
+
+  handleClick = answer => {
+    if (answer == this.props.correct) {
+      this.setState.output("Correct");
+    } else {
+      this.setState.output("Try Again");
+    }
+  };
+
+  render() {
+    return (
+      <View
+        style={{
+          backgroundColor: "#3498db",
+          position: "absolute",
+          layoutOrigin: [0.5, 0.5, 0.5],
+          width: 1,
+          height: 0.5,
+          transform: [{ rotateY: this.props.rotateY }, { translateZ: -3 }],
+          opacity: 1
+        }}
+      >
+        <Text style={this.textStyle}>{this.props.question}</Text>
+        <VrButton onClick={this.handleClick(answer1)}>
+          <Text style={this.textStyle}>{this.props.answer1}</Text>
+        </VrButton>
+
+        <Text style={this.textStyle}>{this.props.answer2}</Text>
+        <Text style={this.textStyle}>{this.props.answer3}</Text>
+      </View>
+    );
+  }
+}
