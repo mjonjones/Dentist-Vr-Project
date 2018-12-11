e80e715a2001e676915ad9df52cce3cf0aa7d31f
Masroor-Unar <45050517+Masroor-Unar@users.noreply.github.com>
2018-12-11 11:04:35 +0000

    Fixed swtich to English

--- RouteEnglish.js
+++ RouteEnglish.js
@@ -3,7 +3,7 @@ import { asset, Text, View, VrButton, Sound, Pano } from "react-vr";
 import { withRouter } from "react-router";
 import Welsh from "./Welsh";
 
-class RouteButton extends React.Component {
+class RouteEnglish extends React.Component {
   constructor(props) {
     super();
     this.state = {};
@@ -15,7 +15,6 @@ class RouteButton extends React.Component {
   }
 
   render() {
-    console.log(this.props);
     return (
       // Styling
       <View
@@ -27,21 +26,18 @@ class RouteButton extends React.Component {
           height: 0.15,
           transform: [{ rotateY: this.props.rotateY }, { translateZ: -3 }],
           borderRadius: 0.07
-        }}>
+        }}
+      >
         <VrButton
           onClick={() => {
             this.props.history.push("/Welsh");
           }}
         >
-          <Text
-            style={this.textStyle}
-          >
-            {this.props.link}
-          </Text>
+          <Text style={this.textStyle}>Cymraeg</Text>
         </VrButton>
       </View>
     );
   }
 }
 
-export default withRouter(RouteButton);
+export default withRouter(RouteEnglish);
